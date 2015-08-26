3741a2277fec61cfd6dcacb4da89803ecbb220e0
chrisb <chrisb@netflix.com>
2015-08-26 08:44:44 -0700

    replace tabs with spaces

--- src/test/groovy/com/anotherchrisberry/spock/extensions/retry/RetryOnFailureSpec.groovy
+++ src/test/groovy/com/anotherchrisberry/spock/extensions/retry/RetryOnFailureSpec.groovy
@@ -4,33 +4,33 @@ import spock.lang.Specification
 
 @RetryOnFailure(times=3)
 class RetryOnFailureSpec extends Specification {
-	
-	static Integer classLevelTries = 0
+    
+    static Integer classLevelTries = 0
     static Integer methodLevelTries = 0
     static Integer thrownTries = 0
 
-	void 'class level test'() {
-		when:
-		if (classLevelTries < 3) {
+    void 'class level test'() {
+        when:
+        if (classLevelTries < 3) {
             classLevelTries++
-			throw new RuntimeException("have not tried enough times ($classLevelTries)")
-		}
+            throw new RuntimeException("have not tried enough times ($classLevelTries)")
+        }
 
-		then:
+        then:
         classLevelTries == 3
-	}
+    }
 
-	@RetryOnFailure(times=5)
-	void 'method level test'() {
-		when:
-		if (methodLevelTries < 5) {
+    @RetryOnFailure(times=5)
+    void 'method level test'() {
+        when:
+        if (methodLevelTries < 5) {
             methodLevelTries++
-			throw new RuntimeException("have not tried enough times ($methodLevelTries)")
-		}
+            throw new RuntimeException("have not tried enough times ($methodLevelTries)")
+        }
 
-		then:
+        then:
         methodLevelTries == 5
-	}
+    }
 
     @RetryOnFailure(times=7)
     void 'expect thrown is okay'() {
